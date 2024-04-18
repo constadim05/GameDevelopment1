d2165dc72683a417d73d490acae6acc27628c798
Costa <constadim05@gmail.com>
2024-04-18 16:04:36 +1000

    18/04

--- Assets/Scripts/fireBullet.cs
+++ Assets/Scripts/fireBullet.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class fireBullet : MonoBehaviour
 {
@@ -8,12 +9,20 @@ public class fireBullet : MonoBehaviour
     public float timeBetweenBullets = 0.15f;
     public GameObject projectile;
 
+    public Slider playerAmmoSlider;
+    public int maxRounds;
+    public int startingRounds;
+    int remainingRounds;
+
     float nextBullet;
 
     // Start is called before the first frame update
     void Awake()
     {
         nextBullet = 0f;
+        remainingRounds = startingRounds;
+        playerAmmoSlider.maxValue = maxRounds;
+        playerAmmoSlider.value = remainingRounds;
     }
 
     // Update is called once per frame
@@ -21,7 +30,7 @@ public class fireBullet : MonoBehaviour
     {
         playerController myPlayer = transform.root.GetComponent<playerController>();
 
-        if(Input.GetAxisRaw("Fire1")>0 && nextBullet < Time.time)
+        if(Input.GetAxisRaw("Fire1")>0 && nextBullet < Time.time && remainingRounds>0)
         {
             nextBullet = Time.time + timeBetweenBullets;
             Vector3 rot;
@@ -32,7 +41,9 @@ public class fireBullet : MonoBehaviour
             else rot = new Vector3(0, 90, 0);
 
             Instantiate(projectile, transform.position, Quaternion.Euler(rot));
-            
+
+            remainingRounds -= 1;
+            playerAmmoSlider.value = remainingRounds;
         }
     }
 }
--- Assets/Scripts/playerHealth.cs
+++ Assets/Scripts/playerHealth.cs
@@ -1,6 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public class playerHealth : MonoBehaviour
 {
@@ -8,23 +8,43 @@ public class playerHealth : MonoBehaviour
     float currentHealth;
 
     public GameObject playerDeathFX;
+
+    //HUD
+    public Slider playerHealthSlider;
+    public Image damageScreen;
+    Color flashColor = new Color(255f, 255f, 255f, 1f);
+    float flashSpeed = 5f;
+    bool damaged = false;
     
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = fullHealth;
+        playerHealthSlider.maxValue = fullHealth;
+        playerHealthSlider.value = currentHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //are we hurt?
+        if (damaged)
+        {
+            damageScreen.color = flashColor;
+        }
+        else
+        {
+            damageScreen.color = Color.Lerp(damageScreen.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
     }
 
     public void addDamage(float damage)
     {
         currentHealth -= damage;
-        if(currentHealth <= 0)
+        playerHealthSlider.value = currentHealth;
+        damaged = true;
+        if (currentHealth <= 0)
         {
             makeDead();
         }
@@ -34,6 +54,7 @@ public class playerHealth : MonoBehaviour
     {
         Instantiate(playerDeathFX, transform.position, Quaternion.Euler(new Vector3(-90, 0, 0)));
         //Destroy(gameObject);
+        damageScreen.color = flashColor;
         gameObject.SetActive(false);
     }
 }
