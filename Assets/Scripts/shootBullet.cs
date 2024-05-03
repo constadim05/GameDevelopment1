950a1e7404ad8613975ab526d2e07bca6e42669c
Costa <constadim05@gmail.com>
2024-05-03 14:05:37 +1000

    3/5

--- Assets/Scripts/shootBullet.cs
+++ Assets/Scripts/shootBullet.cs
@@ -4,38 +4,83 @@ using UnityEngine;
 
 public class shootBullet : MonoBehaviour
 {
-
     public float range = 10f;
     public float damage = 5f;
-
-    Ray shootRay;
-    RaycastHit shootHit;
-
+    private Ray shootRay;
+    private RaycastHit shootHit;
     public LayerMask shootableMask;
-
-    LineRenderer gunLine;
-
-    public static object Raycast { get; private set; }
+    private LineRenderer gunLine;
 
     // Start is called before the first frame update
     void Awake()
     {
-        //shootableMask = LayerMask.GetMask("Shootable");
+        shootableMask = LayerMask.GetMask("Shootable");
         gunLine = GetComponent<LineRenderer>();
-        shootRay.origin = transform.position;
-        shootRay.direction = transform.forward;
+        shootRay = new Ray(transform.position, transform.forward);
         gunLine.SetPosition(0, transform.position);
-        //gunLine.enabled = false;
+        gunLine.enabled = true; // Enable the gunLine by default
     }
-    private void Update()
+
+    void Update()
     {
+        RenderGunLine();
+
         if (Input.GetButtonDown("Fire1"))
         {
-            Shoot(); // Call the Shoot method when Fire1 button is pressed
+           // Debug.Log("Fire1 button pressed");
+
+            Shoot();
         }
     }
 
-     void Shoot()
+    void Shoot()
+    {
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+
+        if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
+        {
+            //Debug.Log("Raycast hit something");
+           // Debug.Log("Hit object name: " + shootHit.collider.gameObject.name);
+
+            if (shootHit.collider.tag == "Enemy")
+            {
+               // Debug.Log("Hit an enemy");
+
+                enemyHealth theEnemyHealth = shootHit.collider.GetComponent<enemyHealth>();
+                theEnemyHealth.addDamage(damage);
+                theEnemyHealth.damageFX(shootHit.point, -shootRay.direction);
+            }
+            
+        }
+    }
+
+    void RenderGunLine()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            shootRay.origin = transform.position;
+            shootRay.direction = transform.forward;
+
+            if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
+            {
+                gunLine.SetPosition(1, shootHit.point);
+            }
+            else
+            {
+                gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+            }
+        }
+        else
+        {
+            gunLine.SetPosition(0, transform.position);
+            gunLine.SetPosition(1, transform.position);
+        }
+    }
+}
+
+/* void Shoot()
+{
     {
         if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
         {
@@ -43,48 +88,21 @@ public class shootBullet : MonoBehaviour
             if (shootHit.collider.tag == "Enemy")
             {
                 enemyHealth theEnemyHealth = shootHit.collider.GetComponent<enemyHealth>();
-    theEnemyHealth.addDamage(damage);
+                theEnemyHealth.addDamage(damage);
                 theEnemyHealth.damageFX(shootHit.point, -shootRay.direction);
             }
 
-//hit an enemy goes here
-
-gunLine.SetPosition(1, shootHit.point);
+            gunLine.SetPosition(1, shootHit.point);
         }
         else
         {
-            gunLine.SetPosition(1, shootRay.origin + shootRay.direction* range);
-        }
-    } 
-}
-      
-    
-    
-    /* void Shoot()
-    {
-        {
-            if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
-            {
-                Debug.Log(shootHit.collider.gameObject.name);
-                if (shootHit.collider.tag == "Enemy")
-                {
-                    enemyHealth theEnemyHealth = shootHit.collider.GetComponent<enemyHealth>();
-                    theEnemyHealth.addDamage(damage);
-                    theEnemyHealth.damageFX(shootHit.point, -shootRay.direction);
-                }
-
-                gunLine.SetPosition(1, shootHit.point);
-            }
-            else
-            {
-                Debug.Log("Raycast didn't hit anything"); // Add this line to check if the ray is not hitting anything
-                gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
-            }
+            Debug.Log("Raycast didn't hit anything"); // Add this line to check if the ray is not hitting anything
+            gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
         }
     }
 }
-    */
-    
-    
-  
-        
+}
+*/
+
+
+
