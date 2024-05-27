feb38d3eb2769fb7fab3c29cf46d076fc12385ce
Costa <constadim05@gmail.com>
2024-05-28 09:47:42 +1000

    dev/changes1 28/05
    added menu package and fixed ammo pickup script

--- Assets/Scripts/ammoPickupController.cs
+++ Assets/Scripts/ammoPickupController.cs
@@ -4,25 +4,50 @@ using UnityEngine;
 
 public class ammoPickupController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+//    // Start is called before the first frame update
+//    void Start()
+//    {
         
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-    }
+//    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Player")
-        {
-            other.GetComponentInChildren<fireBullet>().reload();
-            Destroy(transform.root.gameObject);
-        }
-    }
+//    void OnTriggerEnter(Collider other)
+//    {
+//        if(other.tag == "Player")
+//        {
+//            other.GetComponentInChildren<fireBullet>().reload();
+//            Destroy(transform.root.gameObject);
+//        }
+//    }
+//}
+
+public float ammoAmount;
+public AudioClip ammoPickupSound;
+
+// Start is called before the first frame update
+void Start()
+{
+
 }
 
+// Update is called once per frame
+void Update()
+{
+
+}
+
+void OnTriggerEnter(Collider other)
+{
+    if (other.tag == "Player")
+    {
+        other.GetComponent<playerHealth>().addHealth(ammoAmount);
+        Destroy(transform.root.gameObject);
+        AudioSource.PlayClipAtPoint(ammoPickupSound, transform.position, 0.15f);
+    }
+}
+}
