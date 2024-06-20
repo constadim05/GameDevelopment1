fa4b465bc43c0ea9c0d3b76db56e16104dfd8e2b
Costa <constadim05@gmail.com>
2024-06-20 14:19:57 +1000

    dev/ShootBulletChange
    changed shoot bullet back so it works in local play

--- Assets/Scripts/player1/shootBullet.cs
+++ Assets/Scripts/player1/shootBullet.cs
@@ -1,30 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
 
-public class shootBullet : MonoBehaviour, IPunObservable
+public class shootBullet : MonoBehaviour
 {
     public float range = 10f;
     public float damage = 5f;
     public float fireRate = 0.5f; // Adjust this to control the rate of fire
 
-    private float nextFireTime;
-    private int shootableMask;
-    private LineRenderer gunLine;
-    private PhotonView photonView;
+
+    float nextFireTime;
+    int shootableMask;
+    LineRenderer gunLine;
 
     void Awake()
     {
         shootableMask = LayerMask.GetMask("Shootable");
         gunLine = GetComponent<LineRenderer>();
-        photonView = GetComponent<PhotonView>();
     }
 
     void Update()
     {
-        if (!photonView.IsMine) return;
-
         if (Time.time >= nextFireTime)
         {
             // Check for shooting input (if applicable)
@@ -34,12 +30,6 @@ public class shootBullet : MonoBehaviour, IPunObservable
     }
 
     void Shoot()
-    {
-        photonView.RPC("RPC_Shoot", RpcTarget.All);
-    }
-
-    [PunRPC]
-    void RPC_Shoot()
     {
         Ray shootRay = new Ray(transform.position, transform.forward);
         RaycastHit shootHit;
@@ -69,29 +59,5 @@ public class shootBullet : MonoBehaviour, IPunObservable
 
         // Set the next fire time based on fire rate
         nextFireTime = Time.time + fireRate;
-
-        // Enable the gunLine to make it visible and then disable it after a short delay
-        StartCoroutine(ShowGunLine());
-    }
-
-    IEnumerator ShowGunLine()
-    {
-        gunLine.enabled = true;
-        yield return new WaitForSeconds(0.1f); // Show the line for a short time
-        gunLine.enabled = false;
-    }
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
-        {
-            // Send whether the gunLine is enabled or not
-            stream.SendNext(gunLine.enabled);
-        }
-        else
-        {
-            // Receive whether the gunLine is enabled or not
-            gunLine.enabled = (bool)stream.ReceiveNext();
-        }
     }
 }
